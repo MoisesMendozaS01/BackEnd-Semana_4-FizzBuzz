2b5fb1bf467a15cf9e0cad14b8d8aafeff01126f
= <=>
2022-05-11 11:37:03 -0500

    Paso 12: Agregando prueba de unidad para FizzbuzzService

---
+++ test/lib/services/FizzbuzzService.test.js
@@ -0,0 +1,25 @@
+const Reader = require("./../../../../BackEnd-Semana_4-FizzBuzz/lib/utils/Reader")
+const explorers = Reader.readJsonFile("./explorers.json");
+
+const FizzbuzzService = require('./../../../../BackEnd-Semana_4-FizzBuzz/lib/services/FizzbuzzService')
+
+describe('Unit Test for FizzbuzzzService',()=>{
+    test('Parte 1: prueba de funcion FizzbuzzService',()=>{
+        const explorer1 = {name: "Explorer1", score: 1}
+        const ExplorerTrick = FizzbuzzService.applyValidationInExplorer(explorer1);
+
+        const explorer3 = {name: "Explorer3", score: 3}
+        const ExplorerTrick2 = FizzbuzzService.applyValidationInExplorer(explorer3)
+
+        const explorer5 = {name: "Explorer5", score: 5}
+        const ExplorerTrick3 = FizzbuzzService.applyValidationInExplorer(explorer5)
+
+        const explorer15 = {name: "Explorer15", score: 15}
+        const ExplorerTrick4 = FizzbuzzService.applyValidationInExplorer(explorer15)
+
+        expect(ExplorerTrick.trick).tobe(1)
+        expect(ExplorerTrick2.trick).tobe("FIZZ")
+        expect(ExplorerTrick3.trick).tobe("BUZZ")
+        expect(ExplorerTrick4.trick).tobe("FIZZBUZZ")
+    })
+})
